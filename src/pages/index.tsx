359275156ce63a58f3bf9171a83b03b65cc2fbb8
Dinar Amalia <passyah@studioalva.co>
2023-09-12 22:28:14 +0700

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,50 @@
 import { createClient } from "next-sanity";
-import imageUrlBuilder from "@sanity/image-url";
 import {
   Box,
   Input,
   InputGroup,
   InputRightElement,
   Text,
+  Image,
+  Flex,
 } from "@chakra-ui/react";
 import { Search2Icon } from "@chakra-ui/icons";
 import { useState, useEffect } from "react";
-import SHARE from "@assets/share.png";
-import { NextRouter, useRouter } from "next/router";
-import Navbar from "@src/components/layout/navbar";
 import ProductCard from "@src/components/home/product-card";
-import create from 'zustand';
-
-
-const useStore = create((set:any) => ({
-  keyword: '',
-  setKeyword: (keyword:any) =>
-    set((state:any) => ({
-      ...state,
-      keyword,
-    })),
-}));
+import X from "@assets/close.png";
 
 export default function Home({ data }: any) {
-  const router = useRouter() as NextRouter;
-  const keyword = useStore((state:any) => state.keyword);
-  const setKeyword = useStore((state:any) => state.setKeyword);
-  const builder = imageUrlBuilder(client);
-  const urlFor = (source: any) => {
-    return builder.image(source);
-  };
-
+  const propsData = data.sort(sort_by_id())
+  const [keyword, setKeyword] = useState("");
   const [result, setResult] = useState([]);
 
-  const resultKey = () => {
-    if (keyword) {
-      return data.filter((e: any) => {
-        const y = e.title
-          .toLowerCase()
-          .includes(keyword.toLowerCase());
-        return setResult(y)
-      });
-    } else {
-      return setResult(data);
-    }
+  function sort_by_id() {
+    return function (elem1:any, elem2:any) {
+      if (elem1.number < elem2.number) {
+        return -1;
+      } else if (elem1.id > elem2.id) {
+        return 1;
+      } else {
+        return 0;
+      }
+    };
   }
 
-  console.log(result);
-  
+  useEffect(() => {
+    setResult(
+      keyword
+        ? propsData.filter((e: any) => {
+            const y = e.title.toLowerCase().includes(keyword.toLowerCase());
+            const x = e.number
+              .toString()
+              .toLowerCase()
+              .includes(keyword.toString().toLowerCase());
 
+            return y || x;
+          })
+        : propsData
+    );
+  }, [keyword, propsData]);
 
   return (
     <>
@@ -61,23 +54,30 @@ export default function Home({ data }: any) {
             value={keyword}
             onChange={(e) => setKeyword(e.target.value)}
             bgColor={"white"}
-            placeholder="mau cari apa ?"
+            placeholder="cari nomer / nama disini ?"
+            border={"1px"}
+            borderColor={"blue.600"}
           />
           <InputRightElement>
             <Search2Icon />
           </InputRightElement>
         </InputGroup>
-        <Text mb="8px">Value: {keyword}</Text>
       </Box>
-      {/* Product Name */}
-      {/* <ProductCard data={result} /> */}
-      {result.map((item: any) => {
-        return <Text key={item._id}>{item.title}</Text>;
-      })}
-
-      {/* {data.map((item: any) => {
-        return <Text key={item._id}>{item.title}</Text>;
-      })} */}
+      {/* Product Number & Name */}
+      {result.length > 0 ? (
+        <ProductCard data={result} />
+      ) : (
+        <Flex
+          mt={"30%"}
+          gap={10}
+          flexDir={"column"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <Image src={X.src} w={"80px"} h={"80px"} alt="not-found" />
+          <Text>ups coba kata kunci lain</Text>
+        </Flex>
+      )}
     </>
   );
 }
