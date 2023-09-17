ce150a99e88062c371f7cdf2270f37e376137633
Dinar Amalia <passyah@studioalva.co>
2023-09-17 20:42:09 +0700

    update available

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -94,7 +94,7 @@ const client = createClient({
 });
 
 export async function getStaticProps() {
-  const data = await client.fetch(`*[_type == "testing"]`);
+  const data = await client.fetch(`*[_type == "shopee"]`);
 
   return {
     props: {
