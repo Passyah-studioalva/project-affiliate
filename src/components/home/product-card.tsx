9d190047e8e6e2e984b9279f0895a37d548b2710
Passyah Raffi <passyah@studioalva.co>
2023-11-05 19:47:49 +0700

    update new feature

--- src/components/home/product-card.tsx
+++ src/components/home/product-card.tsx
@@ -7,29 +7,52 @@ export default function ProductCard({ data }: any) {
     <Grid gap={5} mt={10} mb={16}>
       {data?.map((item: any) => {
         return (
-          <Link
-            key={item._id}
-            href={item.link ? item.link : item.slug.current}
-            target={item.link ? "_blank" : ""}
-            rel={item.link ? "noopener noreferrer" : ""}
-          >
-            <Flex
-              p={3}
-              rounded={"md"}
-              alignItems={"center"}
-              justifyContent={"space-between"}
-              fontWeight={500}
-              className="product-button-link"
-            >
-              <Text letterSpacing={"0.5px"}>{item.number}.</Text>
-              <Text letterSpacing={"0.5px"} textAlign={"center"}>
-                {item.title}
-              </Text>
-              <Flex justifyContent={"end"} boxSize={"25px"}>
-                <Image src={SHARE.src} w={"20px"} h={"20px"} alt="share" />
-              </Flex>
-            </Flex>
-          </Link>
+          <>
+            {item.link ? (
+              <Link
+                key={item._id}
+                href={item.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Flex
+                  p={3}
+                  rounded={"md"}
+                  alignItems={"center"}
+                  justifyContent={"space-between"}
+                  fontWeight={500}
+                  className="product-button-link"
+                >
+                  <Text letterSpacing={"0.5px"}>{item.number}.</Text>
+                  <Text letterSpacing={"0.5px"} textAlign={"center"}>
+                    {item.title}
+                  </Text>
+                  <Flex justifyContent={"end"} boxSize={"25px"}>
+                    <Image src={SHARE.src} w={"20px"} h={"20px"} alt="share" />
+                  </Flex>
+                </Flex>
+              </Link>
+            ) : (
+              <Link key={item._id} href={item.slug.current}>
+                <Flex
+                  p={3}
+                  rounded={"md"}
+                  alignItems={"center"}
+                  justifyContent={"space-between"}
+                  fontWeight={500}
+                  className="product-button-link"
+                >
+                  <Text letterSpacing={"0.5px"}>{item.number}.</Text>
+                  <Text letterSpacing={"0.5px"} textAlign={"center"}>
+                    {item.title}
+                  </Text>
+                  <Flex justifyContent={"end"} boxSize={"25px"}>
+                    <Image src={SHARE.src} w={"20px"} h={"20px"} alt="share" />
+                  </Flex>
+                </Flex>
+              </Link>
+            )}
+          </>
         );
       })}
     </Grid>
